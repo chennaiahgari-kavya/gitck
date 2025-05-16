f933b2379298cf9ec1c52fbf118cef8db87c2340
ckavya <ckavyar@gmail.com>
2025-05-16 20:15:01 +0530

    functions

---
+++ 3-script.js
@@ -0,0 +1,48 @@
+function books()  // function declaration
+{
+let bookNames=["java","c-programming","operating system","web technlogy"];
+console.log(bookNames);
+console.log("%c bookNames", "color:blue");
+let stuid=[10,11,12,13,14,15];
+console.log(stuid);
+
+let stuName=["kavya","minnu","sree","kranthi"];
+console.log(stuName);
+}
+books()  // function caling
+
+function branches()   // function declaration
+{
+let branchName=["cse","EEE","ECE","CIVIL"];
+greencolor()
+console.log(branchName);
+let clgName=["TECH","CVRT","SRIT","SVCE","SVIT"];
+console.log(clgName);
+let HODS=["bhanu","bramiah","srinu","naveen"];
+console.log(HODS);
+}
+branches()   // function caling
+function greencolor()
+{
+    console.log("%c bookNames", "color:blue");
+    console.log( "%c        " ,"background-color:green")
+}
+greencolor()
+function redcolor()
+{
+    console.log("%c bookNames", "color:blue");
+    console.log( "%c        " ,"background-color:red")
+}
+redcolor()
+function blackcolor()
+{
+    console.log("%c bookNames", "color:black");
+    console.log( "%c        " ,"background-color:black")
+}
+blackcolor()
+function orangecolor()
+{
+    console.log("%c bookNames", "color:orange");
+    console.log( "%c        " ,"background-color:orange")
+}
+orangecolor()
