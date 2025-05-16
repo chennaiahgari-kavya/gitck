8fe05a82cb0d0767746d90e0b9cd65db84569ecb
ckavya <ckavyar@gmail.com>
2025-05-16 17:10:33 +0530

    objects and arrays

---
+++ 2-script.js
@@ -0,0 +1,43 @@
+//arrays
+// let id = [1, 2, 3, 4, 5]; //the id contains only numbers
+// let Name = ["kavya", "minnu", "sree"]; //the name contains strings
+// let mixed = ["kavya", 1, 2, [null, undefined]]; //the mixed variable contains strings,numbers and list or arrays
+// console.log(id);
+// console.log(Name);
+// console.log(mixed);
+// console.log(id[3]);
+// console.log(Name[0]);
+// var nameFrommixedArray = mixed[3];
+// console.log(nameFrommixedArray);
+// var Namemixeda = mixed[0];
+// console.log(Namemixeda);
+//  let mbooks = ["kranthi", "minnu", "sree", 1,true, null, [1, 2, 3]];
+//  console.log(mbooks);
+//  console.log(mbooks[6][1]);
+// console.log(mbooks[5])
+//objects---->key value pair
+let car = {
+    brand: "honda",
+    year: 2020,
+    model: "cici",
+}
+console.log(car);
+console.log(car.brand);
+let person = {
+   peru:"reddy",
+    age: 26,
+    city: "kadapa",
+    village: "tadipatri",
+    habbies: ["reading", "playig", "singing"],
+    famdetails: ["father", "mother", "bro"],
+    venue:{ city:"atp",
+        hName:"raviteja",
+        time:10,
+
+    }
+    
+}
+console.log(person.age);
+console.log(person.habbies[1]);
+console.log(person.famdetails[1]);
+console.log(person.venue.city);
